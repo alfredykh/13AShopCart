1fb2b0e091e0fd4fa5129bee5e91f1d59197ef4e
alfredykh <alfredykh@users.noreply.github.com>
2019-04-04 10:01:14 +0800

    Add project files.

---
+++ 13AShopCart/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using _13AShopCart.DB;
+using _13AShopCart.Models;
+using System.Diagnostics;
+
+namespace _13AShopCart.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index(string Username, string Password)
+        {
+            if (Username == null) return View();
+
+            User user = UserData.GetUserByUsername(Username);
+            if (user == null) return View();
+            if (user.Password != Password)
+                return View();
+
+            string sessionId = SessionData.CreateSession(user.UserId);
+            return RedirectToAction("Products", "Login", new { sessionId });
+        }
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+    }
+}
---
+++ 13AShopCart/DB/UserData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using _13AShopCart.Models;
+
+namespace _13AShopCart.DB
+{
+    public class UserData : Data
+    {
+        public static User GetUserByUsername(string username)
+        {
+            User user = null;
+
+            using (SqlConnection conn = new SqlConnection(Data.connectionString))
+            {
+                conn.Open();
+
+                string sql = @"SELECT UserId, Username, Password from User1
+                             WHERE Username = '" + username + "'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    user = new User()
+                    {
+                        UserId = (int)reader["UserId"],
+                        Password = (string)reader["Password"]
+                    };
+                }
+            }
+            return user;
+        }
+
+        public static User GetUserBySessionId(string SessionId)
+        {
+            User user = null;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string q = @"SELECT User.UserId, User.Firstname, 
+                    User.Lastname, FROM User,
+                           AND User.SessionId = '" + SessionId + "'";
+
+                SqlCommand cmd = new SqlCommand(q, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    user = new User()
+                    {
+                        UserId = (int)reader["UserId"],
+                        FirstName = (string)reader["FirstName"],
+                        LastName = (string)reader["LastName"],
+                    };
+                }
+            }
+
+            return user;
+        }
+    }
+}
